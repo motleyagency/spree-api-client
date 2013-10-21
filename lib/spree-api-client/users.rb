3dbc5d8e09461cb944ca94b2471e4092e3ea1b53
tonisep <ts@luontocreations.com>
2013-10-21 15:35:40 +0300

    Adds method to create anonymous users

--- lib/spree-api-client/users.rb
+++ lib/spree-api-client/users.rb
@@ -9,10 +9,14 @@ module Spree
         def create_user(options={})
           post("users", options)
         end
-        
+
         def update_users(user_id, options={})
           put("users/#{user_id}", options)
         end
+
+        def create_anonymous_user(options={})
+          post("anonymous_users", options)
+        end        
       end
     end
   end
